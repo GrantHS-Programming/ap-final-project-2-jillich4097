a98efa3b3e30fe3b07d9158b0335c7743af41f0c
jillich4097 <jillich4097@student.pps.net>
2023-05-10 09:10:43 -0700

    commit

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ class Sprite {
 		this.position = position
 		this.velocity = velocity
 		this.height = 150
+		this.lastKey
 	}
 	
 	draw(){
@@ -63,8 +64,14 @@ const keys = {
 	},
 	d: {
 		pressed: false
-	}
+	},
 	w: {
+		pressed: false
+	},
+	ArrowRight: {
+		pressed: false
+	},
+	ArrowLeft: {
 		pressed: false
 	}
 }
@@ -78,16 +85,27 @@ function animate(){
 	enemy.update()
 	
 	player.velocity.x = 0
+	
+	// player movement
 	if (keys.a.pressed && lastKey === 'a' ) {
 		player.velocity.x = -1
 	} else if (keys.d.pressed && lastKey === 'd' ){
 		player.velocity.x = 1
 	}
+	
+	// enemy movement
+	if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft' ) {
+		enemy.velocity.x = -1
+	} else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight' ){
+		enemy.velocity.x = 1
+	}
 }
+
 
 animate()
 
 window.addEventListener('keydown',(event) => {
+	console.log(event.key)
 	switch (event.key) {
 		case 'd' :
 		keys.d.pressed = true
@@ -98,8 +116,19 @@ window.addEventListener('keydown',(event) => {
 		lastKey = 'a'
 		break
 		case 'w' :
-		keys.w.pressed = false
-		lastkey = 'w'
+		player.velocity.y = -10
+		break
+		
+		case 'ArrowRight' :
+		keys.ArrowRight.pressed = true
+		enemy.lastKey = 'ArrowRight'
+		break
+		case 'ArrowLeft' :
+		keys.ArrowLeft.pressed = true
+		enemy.lastKey = 'ArrowLeft'
+		break
+		case 'ArrowUp' :
+		enemy.velocity.y = -10
 		break
 	}
 	console.log(event.key)
@@ -116,7 +145,16 @@ window.addEventListener('keyup',(event) => {
 		keys.w.pressed = false
 		break
 	}
+
+	switch (event.key) {
+		case 'ArrowRight' :
+		keys.ArrowRight.pressed = false
+		break
+		case 'ArrowLeft' :
+		keys.ArrowLeft.pressed = false
+		break
+	}
 	console.log(event.key)
 })
-	
+
 	
